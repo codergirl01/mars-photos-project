89edbd442693151c8103b663397951866602fde4
Wendy Kuo <wendykuowob@gmail.com>
2024-01-01 20:40:36 -0800

    add footer

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mars-photos-project/",
+  base: "/deploy-mars-app/",
   server: {
     port: 3000,
     proxy: {
